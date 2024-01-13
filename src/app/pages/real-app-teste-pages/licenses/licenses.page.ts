80b7a059f3320c5678cbec9b72c386a6259865e8
CommandPromptGamer <46406111+CommandPromptGamer@users.noreply.github.com>
2024-01-13 12:12:35 -0300

    Carregue a lista de licenças dinamicamente

--- src/app/pages/real-app-teste-pages/licenses/licenses.page.ts
+++ src/app/pages/real-app-teste-pages/licenses/licenses.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { DomSanitizer } from '@angular/platform-browser';
 
 import { Browser } from '@capacitor/browser';
 
@@ -9,19 +10,67 @@ import { Browser } from '@capacitor/browser';
 })
 export class LicensesPage implements OnInit {
 
-  constructor() { }
+  protected projects: Project[] = [
+    {
+      friendlyName: "Angular",
+      internalName: "angular",
+      licenseFriendlyName: "MIT License",
+      licenseSPDX: "MIT",
+      url: "https://angular.dev/"
+    },
+    {
+      friendlyName: "Capacitor",
+      internalName: "capacitor",
+      licenseFriendlyName: "MIT License",
+      licenseSPDX: "MIT",
+      url: "https://capacitorjs.com/"
+    },
+    {
+      friendlyName: "Ionic Framework",
+      internalName: "ionic",
+      licenseFriendlyName: "MIT License",
+      licenseSPDX: "MIT",
+      url: "https://ionicframework.com/"
+    }
+  ]
+
+  constructor(private sanitizer: DomSanitizer) { }
 
   ngOnInit() {
   }
 
-  linkButtonClick(event: Event, url: string) {
+  protected getSafeLicenseUrl(fileName: string) {
+    const url = '/assets/licenses/' + fileName + '.txt';
+    return this.sanitizer.bypassSecurityTrustResourceUrl(url);
+  }
+
+  protected linkButtonClick(event: Event, url: string) {
     event.stopImmediatePropagation();
 
     this.openProjectUrl(url);
   }
 
-  openProjectUrl = async (url: string) => {
+  private openProjectUrl = async (url: string) => {
     await Browser.open({ url: url });
   };
 
+}
+
+/**
+ * Project interface
+ * 
+ * This interface defines the parameters each licensed project should have.
+
+ * @param {string} friendlyName - Project's friendly user-facing name
+ * @param {string} internalName - Project's internal name of the project: no spaces or special characters
+ * @param {string} licenseFriendlyName - Friendly name of the project license, user-facing
+ * @param {string} licenseSPDX - The SPDX name of the license (https://spdx.org/licenses/)
+ * @param {string?} url - URL to the project home page (optional)
+**/
+interface Project {
+  friendlyName: string,
+  internalName: string,
+  licenseFriendlyName: string,
+  licenseSPDX: string,
+  url?: string
 }
